9148fc43f453bcf51a4a4284ce3d3e615bbf746e
--global <--global>
2024-05-23 21:50:04 +0300

    website mohid by rustamovv

---
+++ js/loading.js
@@ -0,0 +1,10 @@
+
+// or
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2000; // 2s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
---
+++ js/main.js
@@ -0,0 +1,80 @@
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+// navbar toggle end
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
+// backtop end
+
+// ows start
+$(document).ready(function () {
+  $("#team-carousel").owlCarousel({
+    margin: 30,
+    loop: true,
+    nav: true,
+    navText: ['<img src="images/prev.svg"/>', '<img src="images/next.svg"/>'],
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+  });
+});
+// owl end
+// navbar shrink
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+// navbar shrink end
+
+// light-dark start
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
